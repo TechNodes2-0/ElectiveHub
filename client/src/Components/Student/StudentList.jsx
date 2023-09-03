d0f8f2ba75a2c6b761b6514080fb78de5db5d026
Nishitbariya <85815172+Nishitbaria@users.noreply.github.com>
2023-09-03 15:28:11 +0530

    Admin Page

--- client/src/Components/Student/StudentList.jsx
+++ client/src/Components/Student/StudentList.jsx
@@ -1,3 +1,5 @@
+/** @format */
+
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
@@ -26,14 +28,10 @@ const StudentList = ({ token }) => {
       const response = await axios.get(
         `${import.meta.env.VITE_API_URL}/student/allstudents`,
         {
-
           headers: {
             Authorization: `Bearer ${token}`,
-          }
+          },
         }
-
-
-
       );
       console.log(response.data[0]);
       setStudents(response.data);
@@ -50,167 +48,166 @@ const StudentList = ({ token }) => {
   const confirmDelete = async () => {
     try {
       await axios.delete(
-        `${import.meta.env.VITE_API_URL}/student/DeleteStudent/${selectedStudent.idNumber}`,
+        `${import.meta.env.VITE_API_URL}/student/DeleteStudent/${
+          selectedStudent.idNumber
+        }`,
         {
-
           headers: {
             Authorization: `Bearer ${token}`,
-          }
+          },
         }
 
-          // headers: {
-          //   Authorization: `Bearer ${token}`,
-          // },
-        
+        // headers: {
+        //   Authorization: `Bearer ${token}`,
+        // },
       );
-toast.success("Student deleted successfully!");
-fetchStudents();
+      toast.success("Student deleted successfully!");
+      fetchStudents();
     } catch (error) {
-  console.error(error);
-  toast.error("Failed to delete student.");
-}
-setShowModal(false);
+      console.error(error);
+      toast.error("Failed to delete student.");
+    }
+    setShowModal(false);
   };
 
-const cancelDelete = () => {
-  setSelectedStudent(null);
-  setShowModal(false);
-};
+  const cancelDelete = () => {
+    setSelectedStudent(null);
+    setShowModal(false);
+  };
 
-return (
-  <>
-    <div className="flex flex-col items-center bg-gray-900">
-      <div className="py-10">
-      <h2 className="text-2xl font-bold mb-4 text-white text-center">
-        Student List
-      </h2>
-      <div className="mb-4 ml-3">
-        <div className="relative ">
-          <input
-            type="text"
-            placeholder="Search Students..."
-            value={searchQuery}
-            onChange={handleSearch}
-            className="border border-gray-300 px-3 py-2 pl-8 rounded"
-          />
-          <span className="absolute top-3 left-2">
-            <img
-              src="search.png"
-              alt="Search Icon"
-              className="w-4 h-4 text-gray-500"
-            />
-          </span>
-        </div>
-      </div>
-
-      </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mb-4 px-2">
-        {filteredStudents.map((student) => (
-          <div
-            key={student.id}
-            className="bg-gray-800 border-blue-500 border-2 border-solid rounded-lg tarnsition duration-200 hover:scale-105 shadow p-4 flex flex-col justify-between text-white"
-          >
-            <div>
-              <h3 className="text-lg font-medium">{student.name}</h3>
-              <p className="text-gray-500">ID: {student.idNumber}</p>
-              <p className="text-gray-500">Email: {student.email}</p>
-              <p className="text-gray-500">Phone: {student.phoneNumber}</p>
-            </div>
-            <div className="mt-4">
-              {/* Add buttons for editing and deleting a student */}
-              <Link
-                to={{
-                  pathname: `/edit/${student.idNumber}`,
-                  state: { studentData: student, id: student.idNumber },
-                }}
-                className="bg-blue-500 text-white px-4 py-2 rounded mr-2"
-              >
-                Edit
-              </Link>
-              <Link
-                to={`/add-subject-to-student/${student.idNumber}`}
-                className="bg-green-500 text-white px-4 py-2 rounded mr-2"
-              >
-                View Subjects
-              </Link>
-              <button
-                onClick={() => handleDelete(student)}
-                className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
-              >
-                Delete
-              </button>
+  return (
+    <>
+      <div className="flex flex-col items-center bg-gray-900 h-screen">
+        <div className="py-10">
+          <h2 className="text-2xl font-bold mb-4 text-white text-center">
+            Student List
+          </h2>
+          <div className="mb-4 ml-3">
+            <div className="relative ">
+              <input
+                type="text"
+                placeholder="Search Students..."
+                value={searchQuery}
+                onChange={handleSearch}
+                className="border border-gray-300 px-3 py-2 pl-8 rounded"
+              />
+              <span className="absolute top-3 left-2">
+                <img
+                  src="search.png"
+                  alt="Search Icon"
+                  className="w-4 h-4 text-gray-500"
+                />
+              </span>
             </div>
           </div>
-        ))}
-      </div>
-
-      {showModal && (
-        <div
-          id="popup-modal"
-          tabIndex="-1"
-          className="fixed top-0 left-0 right-0 z-50 p-4 flex items-center justify-center bg-black bg-opacity-50"
-        >
-          <div className="relative bg-white rounded-lg shadow max-w-md">
-            <button
-              type="button"
-              className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 flex items-center justify-center"
-              onClick={cancelDelete}
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mb-4 px-2">
+          {filteredStudents.map((student) => (
+            <div
+              key={student.id}
+              className="bg-gray-800 border-blue-500 border-2 border-solid rounded-lg tarnsition duration-200 hover:scale-105 shadow p-4 flex flex-col justify-between text-white"
             >
-              <svg
-                className="w-3 h-3"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 14 14"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
-                />
-              </svg>
-              <span className="sr-only">Close modal</span>
-            </button>
-            <div className="p-6 text-center bg-gray-900">
-              <svg
-                className="mx-auto mb-4 text-gray-400 w-12 h-12"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 20 20"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M10 11V6m0 0 6-6M10 11h4m-4 0H6"
-                />
-              </svg>
-              <h3 className="mb-5 text-lg font-normal text-white">
-                Are you sure you want to delete this student?
-              </h3>
+              <div>
+                <h3 className="text-lg font-medium">{student.name}</h3>
+                <p className="text-gray-500">ID: {student.idNumber}</p>
+                <p className="text-gray-500">Email: {student.email}</p>
+                <p className="text-gray-500">Phone: {student.phoneNumber}</p>
+              </div>
+              <div className="mt-4">
+                {/* Add buttons for editing and deleting a student */}
+                <Link
+                  to={{
+                    pathname: `/edit/${student.idNumber}`,
+                    state: { studentData: student, id: student.idNumber },
+                  }}
+                  className="bg-blue-500 text-white px-4 py-2 rounded mr-2"
+                >
+                  Edit
+                </Link>
+                <Link
+                  to={`/add-subject-to-student/${student.idNumber}`}
+                  className="bg-green-500 text-white px-4 py-2 rounded mr-2"
+                >
+                  View Subjects
+                </Link>
+                <button
+                  onClick={() => handleDelete(student)}
+                  className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
+                >
+                  Delete
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        {showModal && (
+          <div
+            id="popup-modal"
+            tabIndex="-1"
+            className="fixed top-0 left-0 right-0 z-50 p-4 flex items-center justify-center bg-black bg-opacity-50"
+          >
+            <div className="relative bg-white rounded-lg shadow max-w-md">
               <button
-                onClick={confirmDelete}
-                className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2"
-              >
-                Yes, I'm sure
-              </button>
-              <button
+                type="button"
+                className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 flex items-center justify-center"
                 onClick={cancelDelete}
-                className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10"
               >
-                No, cancel
+                <svg
+                  className="w-3 h-3"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 14 14"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                  />
+                </svg>
+                <span className="sr-only">Close modal</span>
               </button>
+              <div className="p-6 text-center bg-gray-900">
+                <svg
+                  className="mx-auto mb-4 text-gray-400 w-12 h-12"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 20 20"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M10 11V6m0 0 6-6M10 11h4m-4 0H6"
+                  />
+                </svg>
+                <h3 className="mb-5 text-lg font-normal text-white">
+                  Are you sure you want to delete this student?
+                </h3>
+                <button
+                  onClick={confirmDelete}
+                  className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2"
+                >
+                  Yes, I'm sure
+                </button>
+                <button
+                  onClick={cancelDelete}
+                  className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10"
+                >
+                  No, cancel
+                </button>
+              </div>
             </div>
           </div>
-        </div>
-      )}
-    </div>
-  </>
-);
+        )}
+      </div>
+    </>
+  );
 };
 
 export default StudentList;
--- client/src/Pages/AdminPage/Admin.jsx
+++ client/src/Pages/AdminPage/Admin.jsx
@@ -1,11 +1,144 @@
 /** @format */
 
-import React from "react";
+import { React, useState } from "react";
+import SubjectHome from "../../Pages/SubjectHome";
 
 export default function Admin() {
+  const [activeTab, setActiveTab] = useState("Subject");
+
+  const handleTabClick = (tab) => {
+    setActiveTab(tab);
+  };
+
   return (
     <div>
-      <h1>Admin</h1>
+      <div class="bg-gray-50 dark:bg-slate-900">
+        <div class="sticky top-0 inset-x-0 z-20 bg-white border-y px-4 sm:px-6 md:px-8 lg:hidden dark:bg-gray-800 dark:border-gray-700">
+          <div class="flex items-center py-4">
+            <button
+              type="button"
+              class="text-gray-500 hover:text-gray-600"
+              data-hs-overlay="#application-sidebar"
+              aria-controls="application-sidebar"
+              aria-label="Toggle navigation"
+            >
+              <span class="sr-only">Toggle Navigation</span>
+              <svg
+                class="w-5 h-5"
+                width="16"
+                height="16"
+                fill="currentColor"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
+                />
+              </svg>
+            </button>
+
+            <ol
+              class="ml-3 flex items-center whitespace-nowrap min-w-0"
+              aria-label="Breadcrumb"
+            >
+              <li class="flex items-center text-sm text-gray-800 dark:text-gray-400">
+                Application Layout
+                <svg
+                  class="flex-shrink-0 mx-3 overflow-visible h-2.5 w-2.5 text-gray-400 dark:text-gray-600"
+                  width="16"
+                  height="16"
+                  viewBox="0 0 16 16"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M5 1L10.6869 7.16086C10.8637 7.35239 10.8637 7.64761 10.6869 7.83914L5 14"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                  />
+                </svg>
+              </li>
+              <li
+                class="text-sm font-semibold text-gray-800 truncate dark:text-gray-400"
+                aria-current="page"
+              >
+                Dashboard
+              </li>
+            </ol>
+          </div>
+        </div>
+
+        <div
+          id="application-sidebar"
+          class="hs-overlay hs-overlay-open:translate-x-0 -translate-x-full transition-all duration-300 transform hidden fixed top-0 left-0 bottom-0 z-[60] w-64 bg-white border-r border-gray-200 pt-7 pb-10 overflow-y-auto scrollbar-y lg:block lg:translate-x-0 lg:right-auto lg:bottom-0 dark:scrollbar-y dark:bg-gray-800 dark:border-gray-700"
+        >
+          <div class="px-6">
+            <a
+              class="flex-none text-xl font-semibold dark:text-white"
+              href="#"
+              aria-label="Brand"
+            >
+              Admin Dashboard
+            </a>
+          </div>
+
+          <nav
+            class="hs-accordion-group p-6 w-full flex flex-col flex-wrap"
+            data-hs-accordion-always-open
+          >
+            <ul class="space-y-1.5">
+              <li>
+                <a
+                  class="flex items-center gap-x-3.5 py-2 px-2.5 bg-gray-100 text-sm text-slate-700 rounded-md hover:bg-gray-100 dark:bg-gray-900 dark:text-white"
+                  href="javascript:;"
+                  onClick={handleTabClick.bind(this, "dashboard")}
+                >
+                  Dashboard
+                </a>
+              </li>
+
+              <li>
+                <a
+                  class="flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-slate-700 rounded-md hover:bg-gray-100 dark:hover:bg-gray-900 dark:text-slate-400 dark:hover:text-slate-300"
+                  href="javascript:;"
+                  onClick={handleTabClick.bind(this, "Subject")}
+                >
+                  Subject
+                </a>
+              </li>
+              <li>
+                <a
+                  class="flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-slate-700 rounded-md hover:bg-gray-100 dark:hover:bg-gray-900 dark:text-slate-400 dark:hover:text-slate-300"
+                  href="javascript:;"
+                >
+                  Student
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+
+        <div class="w-full h-screen pt-10 px-4 sm:px-6 md:px-8 lg:pl-72 ">
+          {activeTab === "dashboard" && (
+            <h1 class="block text-2xl font-bold text-gray-800 sm:text-3xl dark:text-white">
+              Application Layout: Sidebar using Tailwind CSS
+            </h1>
+          )}
+
+          {activeTab === "Subject" && (
+            <div>
+              <SubjectHome />
+            </div>
+          )}
+
+          {activeTab === "Student" && (
+            <div>
+              <SubjectHome />
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
