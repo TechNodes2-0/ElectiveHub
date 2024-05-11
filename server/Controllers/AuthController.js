33783a10c47b3cb96415cb83abdc3b0c7a3b38f2
Amrit <amritsinghal2211@gmail.com>
2024-05-11 12:50:22 +0530

    added username uniqueness

--- server/Controllers/AuthController.js
+++ server/Controllers/AuthController.js
@@ -4,26 +4,31 @@ const bcrypt = require("bcryptjs");
 
 module.exports.Signup = async (req, res, next) => {
   try {
-    const { email, password, username, createdAt,role } = req.body;
+    const { email, password, username, createdAt, role } = req.body;
     
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-      return res.json({ message: "User already exists" });
+    const existingUserByEmail = await User.findOne({ email });
+    if (existingUserByEmail) {
+      return res.json({ message: "Email already exists" });
     }
-    const user = await User.create({ email, password, username, createdAt ,role});
+
+    const existingUserByUsername = await User.findOne({ username });
+    if (existingUserByUsername) {
+      return res.json({ message: "Username already exists" });
+    }
+
+    const user = await User.create({ email, password, username, createdAt, role });
     const token = createSecretToken(user._id);
     res.cookie("token", token, {
       withCredentials: true,
       httpOnly: false,
     });
-    res
-      .status(201)
-      .json({ message: "User signed in successfully", success: true, token  });
+    res.status(201).json({ message: "User signed up successfully", success: true, token });
     next();
   } catch (error) {
     console.error(error);
   }
 };
+
 
 module.exports.Login = async (req, res, next) => {
   try {
