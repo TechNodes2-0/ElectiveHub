cb5317d923b0850e4df840a20523ea7e64ff2a83
Tanu Agarwal <tanu.ag976@gmailcom>
2024-05-12 19:47:04 +0530

    newsletter enhanced

--- client/src/Pages/HomePage.jsx
+++ client/src/Pages/HomePage.jsx
@@ -17,6 +17,7 @@ export default function Homepage() {
   const [email, setEmail] = useState(""); // State to store the email input
   const [subscribed, setSubscribed] = useState(false); // State to track subscription status
   const [error, setError] = useState(null); // State to handle subscription errors
+  const [placeHolder, setPlaceHolder] = useState(true); //for placeholder
 
   const handleSubscribe = async () => {
     try {
@@ -36,7 +37,8 @@ export default function Homepage() {
       }
     } catch (error) {
       console.log(error);
-      setError("An error occurred while you subscribing to the newsletter.");
+      setEmail(" ");
+      setError("An error occurred while you were subscribing to the newsletter.");
     }
   };
 
@@ -123,13 +125,17 @@ export default function Homepage() {
                 type="text"
                 className="peer block min-h-[auto] w-full rounded border-2 mr-2 bg-transparent py-[0.32rem] px-3 leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none text-neutral-200 placeholder:text-neutral-200 peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
                 id="emailInput"
-                placeholder="Enter your email"
+                placeholder={placeHolder ? "": "Enter your email"}
+                onFocus={() =>setPlaceHolder(false)}
+                onBlur={() => setEmail("")}
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
               />
               <label
                 htmlFor="emailInput"
-                className="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-slate-200 transition-all duration-200 ease-out peer-focus:-translate-y-[2.15rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[2.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
+                className={`pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate py-1 leading-[2.15] text-slate-200 transition-all duration-200 ease-out             
+                ${placeHolder || email ? "-translate-y-[2.15rem] scale-[0.8] text-primary" : ""} ${!email && !placeHolder ? "" : "opacity-0 translate-y-[2.15rem] scale-[0.8]"}
+                 motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary`}
               >
                 Enter your email
               </label>
@@ -143,7 +149,9 @@ export default function Homepage() {
               >
                 {subscribed ? "Subscribed" : "Subscribe"}
               </button>
+              
             </div>
+            {error && <p className="text-red-500 pt-2">{error}</p>}
 
             <div className="mt-5">
               <h1 className="text-lg lg:text-xl text-blue-300 font-medium mb-2">Follow Us On</h1>
@@ -155,7 +163,7 @@ export default function Homepage() {
               </div>
             </div>
           </div>
-          {error && <p className="text-red-500">{error}</p>}
+         
         </div>
       </section>
 
