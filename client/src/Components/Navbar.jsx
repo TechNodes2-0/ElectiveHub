f31c57bcf7511ff8eda1659c3b98915c872bc7d9
Hrishita Paul <hrishita.paul03@gmail.com>
2024-05-14 19:33:43 -0700

    fix:Student commit Navbar

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -227,15 +227,6 @@ export default function Navbar() {
                     </Link>
                   </li>
                   <li>
-                    <Link
-                      to="/Admin"
-                      className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent"
-                      aria-current="page"
-                    >
-                      Admin
-                    </Link>
-                  </li>
-                  <li>
                   <Link to="/Home">
                   <span className="block py-1 pl-3 pr-4 rounded bg-gray-800 text-white">
                       Welcome, {username}
