46e350c6a7f4dc6c321e9983833877ed4d1e8f4b
manpreet244 <mk9487371@gmail.com>
2024-05-15 10:32:10 +0530

    Footer button bug fixed

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -18,7 +18,9 @@ export default function Footer() {
               alt="Elective Subject Selector and Manager Logo"
             />
             <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-              ElectiveHub
+             
+            <Link to="/" className="hover:underline"> ElectiveHub
+            </Link>
             </span>
           </a>
           <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
@@ -45,11 +47,11 @@ export default function Footer() {
           </ul>
         </div>
         <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
           © 2023{" "}
-          <Link to="/" class="hover:underline">
-            ElectiveHub™
-          </Link>
+         
+            ElectiveHubs™
+          
           . All Rights Reserved.
         </span>
       </div>
