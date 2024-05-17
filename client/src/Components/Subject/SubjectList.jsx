68fcc582fd2683f46ad9f3ab9a07d7f229bb8e86
Aryan <aryansharma220318@gmail.com>
2024-05-17 21:46:27 +0530

    corrected the delete message

--- client/src/Components/Subject/SubjectList.jsx
+++ client/src/Components/Subject/SubjectList.jsx
@@ -7,7 +7,6 @@ import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { motion } from "framer-motion";
 import BackToTopButton from "../BackToTopButton";
-import { TiDelete } from "react-icons/ti";
 
 const ElectiveSubjectPage = () => {
   const { token } = useContext(AuthContext);
@@ -194,7 +193,8 @@ const ElectiveSubjectPage = () => {
                   d="M10 11V6m0 0 6-6M10 11h6m-6 0H4m6 0v6m0-6V4"
                 />
               </svg> */}
-              <TiDelete />
+              <svg xmlns="http://www.w3.org/2000/svg" className="mx-auto mb-4 text-gray-400 w-12 h-12" viewBox="0 0 32 32" id="delete"><path d="M24.2,12.193,23.8,24.3a3.988,3.988,0,0,1-4,3.857H12.2a3.988,3.988,0,0,1-4-3.853L7.8,12.193a1,1,0,0,1,2-.066l.4,12.11a2,2,0,0,0,2,1.923h7.6a2,2,0,0,0,2-1.927l.4-12.106a1,1,0,0,1,2,.066Zm1.323-4.029a1,1,0,0,1-1,1H7.478a1,1,0,0,1,0-2h3.1a1.276,1.276,0,0,0,1.273-1.148,2.991,2.991,0,0,1,2.984-2.694h2.33a2.991,2.991,0,0,1,2.984,2.694,1.276,1.276,0,0,0,1.273,1.148h3.1A1,1,0,0,1,25.522,8.164Zm-11.936-1h4.828a3.3,3.3,0,0,1-.255-.944,1,1,0,0,0-.994-.9h-2.33a1,1,0,0,0-.994.9A3.3,3.3,0,0,1,13.586,7.164Zm1.007,15.151V13.8a1,1,0,0,0-2,0v8.519a1,1,0,0,0,2,0Zm4.814,0V13.8a1,1,0,0,0-2,0v8.519a1,1,0,0,0,2,0Z"></path></svg>
+              
               <h3 className="mb-5 text-lg font-normal text-gray-500">
                 Are you sure you want to delete this subject?
               </h3>
