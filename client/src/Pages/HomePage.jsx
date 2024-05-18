da0f43b80cd46cd48e9959ace8578b8f8b58b6cd
Yash Suthar <98970491+Yash636261@users.noreply.github.com>
2024-05-18 10:14:23 +0530

    Update HomePage.jsx

--- client/src/Pages/HomePage.jsx
+++ client/src/Pages/HomePage.jsx
@@ -87,9 +87,9 @@ export default function Homepage() {
     <div className="min-h-screen">
       <Navbar></Navbar>
 
-      <section class="bg-white dark:bg-gray-900 ">
+      <section class="bg-white dark:bg-gray-900 min-h-screen">
 
-        <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+        <div class="grid max-w-screen-xl px-4 py-8 mx-auto my-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
           <div class="mr-auto place-self-center lg:col-span-7  home-section  ">
             <h1 class="sub-selection max-w-2xl mb-4 text-4xl font-bold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
               Subject Selection tool for elective subject
