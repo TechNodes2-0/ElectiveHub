7b00f5584ff6a475b38090f9ff0e4410dd046c7b
Yash Suthar <98970491+Yash636261@users.noreply.github.com>
2024-06-18 17:12:46 +0530

    Revert "added model for feedback , controllers to add and get course feedback and their corresponding routes"

--- server/Middleware/AuthMiddleware.js
+++ server/Middleware/AuthMiddleware.js
@@ -3,35 +3,33 @@ const jwt = require("jsonwebtoken");
 const User = require("../Models/UserModel");
 
 module.exports.authMiddleware = (requiredRoles) => {
-  return async (req, res, next) => {
+return async (req, res, next) => {
     try {
-      console.log(requiredRoles);
-      console.log("Headers", req.headers.authorization);
-      var token = req.headers.authorization;
+      console.log(requiredRoles)
+        console.log("Headers",req.headers.authorization);
+      var token =req.headers.authorization;
 
       if (!token) {
-        return res
-          .status(401)
-          .json({ message: "Access denied. No token provided." });
+        return res.status(401).json({ message: "Access denied. No token provided." });
       }
-      token = token.split(" ")[1];
-      jwt.verify(token, process.env.TOKEN_KEY, async (err, decoded) => {
+    token=token.split(" ")[1];
+      jwt.verify(token, process.env.TOKEN_KEY,  async (err, decoded) => {
         if (err) {
           return res.status(401).json({ message: "Invalid token." });
         }
 
-        const user = await User.findById(decoded.id);
-        if (user) req.user = user; // Assuming the JWT payload contains the user information
+
+        const user = await User.findById(decoded.id) ;
+        if (user)  req.user=user;// Assuming the JWT payload contains the user information
         const { role } = user;
         console.log(role);
         //         // console.log(role);
-        if (!hasRequiredRole(role, requiredRoles)) {
-          console.log("error", requiredRoles);
-
-          return res
-            .status(403)
-            .json({ message: "Access denied. Insufficient role." });
-        }
+                if (!hasRequiredRole(role, requiredRoles)) {
+                  console.log("error",requiredRoles);
+                
+                  return res.status(403).json({ message: "Access denied. Insufficient role." });
+                }
+                
 
         next();
       });
@@ -39,8 +37,9 @@ module.exports.authMiddleware = (requiredRoles) => {
       console.error(error);
       res.status(500).json({ message: "Server error." });
     }
-  };
-};
+  }
+
+}
 const hasRequiredRole = (userRole, requiredRoles) => {
   return requiredRoles.includes(userRole);
 };
--- server/Routes/FeedbackRoutes.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const {
-  addFeedback,
-  getFeedback,
-} = require("../Controllers/FeedbackController");
-router.post("/addFeedback", addFeedback);
-router.get("/getFeedback/:subjectCode", getFeedback);
-module.exports = router;
